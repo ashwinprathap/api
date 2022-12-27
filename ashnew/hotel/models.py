8515201aea39714130fea1d9612610493bbecac0
ashwinprathap <110886717+ashwinprathap@users.noreply.github.com>
2022-12-27 12:59:33 +0530

    Add files via upload

---
+++ ashnew/hotel/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+dish_items=[
+    {
+        "id":1,"name":"cb","category":"nonveg","price":120
+    },
+    {
+        "id":2,"name":"cicken curry","category":"nonveg","price":180
+    },
+    {
+        "id":3,"name":"veg-rice","category":"veg","price":50
+    },
+    {
+        "id":4,"name":"veg-curry","category":"veg","price":120
+    },
+    {
+        "id":5,"name":"fried beef","category":"nonveg","price":300
+    },
+
+    
+]
